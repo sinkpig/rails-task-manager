771799598c681af175b23ab55c53f4969fef07e7
Milo Ramirez <sinkpig00@gmail.com>
2022-10-08 15:59:10 -0300

    Finished challenge

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,43 @@
+class TasksController < ApplicationController
+  before_action :find, except: %i[index new create]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path(@task)
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find
+    @task = Task.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # READ ALL
+  get 'tasks', to: 'tasks#index', as: 'index'
+  # CREATE
+  get 'tasks/new', to: 'tasks#new', as: 'create'
+  post 'tasks', to: 'tasks#create'
+  # UPDATE
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch 'tasks/:id', to: 'tasks#update'
+  # DELETE
+  delete 'tasks/:id', to: 'tasks#destroy'
+  # READ ONE
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
 end
